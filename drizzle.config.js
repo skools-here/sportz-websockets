2f55a81fdca1fac8ecced4bcc091b20558eb11ce
skools-here <sajalkulshreshtha9@gmail.com>
2026-03-25 11:32:54 +0530

    Implement db schema

---
+++ drizzle.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+import 'dotenv/config';
+
+export default defineConfig({
+  schema: "./src/db/schema.js",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  verbose: true,
+  strict: true,
+});
